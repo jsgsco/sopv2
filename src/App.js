fd0b802d673ce1f9b1f0892209e8bbabe0140338
Jhon Steven Saavedra <foeblackg@gmail.com>
2021-07-20 19:57:27 -0500

    Change of Sign (Up and In)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Container } from './components/style/Container'
 import Home from './components/pages/Home/Home'
-import Sign from './components/pages/Sign/Sign'
+import SignIn from './components/pages/Sign/SignIn'
 import Error from './components/pages/Error/Error'
 import { AuthProvider } from './context/AuthContext'
 import { SignProvider } from './context/SignContext'
@@ -18,7 +18,7 @@ function App() {
                   <RoutePrivate path="/" exact>
                     <Home />
                   </RoutePrivate>
-                  <Route path="/iniciar-sesion" component={Sign} exact/>
+                  <Route path="/iniciar-sesion" component={SignIn} exact/>
                   <Route path="/**" component={Error} exact/>
               </Switch>
             </Router>
