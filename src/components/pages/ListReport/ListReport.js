24ed11b8165d9ad96f011f06c000315389f3e4da
German Diaz <germanrodrigo.2003@hotmail.com>
2021-07-20 23:26:22 -0500

    tablas 1v

--- src/components/pages/ListReport/ListReport.js
+++ src/components/pages/ListReport/ListReport.js
@@ -1,5 +1,31 @@
+import { Table, TableRow, TableHead, Cell } from "../../style/ListReportStyle";
+
 const ListReport = () => {
-    return ('Desde Lista Objetos');
+    return (
+        <Table>
+            <TableRow>
+                <TableHead>Nombre Completo</TableHead>
+                <TableHead>Fecha de perdida</TableHead>
+                <TableHead>Elemento</TableHead>
+                <TableHead>Descripcion</TableHead>
+                <TableHead>Contacto</TableHead>
+            </TableRow>
+            <TableRow>
+                <Cell>Jhon steven saavedra</Cell>
+                <Cell>24/08/2021</Cell>
+                <Cell>celular</Cell>
+                <Cell>un celular rojo con funda negra y al prenderlo tiene un fondo de pantalla con tonalidades rojas</Cell>
+                <Cell>121221212</Cell>
+            </TableRow>
+            <TableRow>
+                <Cell>German Rodrigo Diaz Olarte</Cell>
+                <Cell>15/09/2021</Cell>
+                <Cell>maleta</Cell>
+                <Cell>un celular rojo con funda negra y al prenderlo tiene un fondo de pantalla con tonalidades rojas</Cell>
+                <Cell>121221212</Cell>
+            </TableRow>
+        </Table>
+    );
 }
  
 export default ListReport;
---
+++ src/components/style/ListReportStyle.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+    border-radius: 2px;
+    border-collapse: collapse;
+    border: solid 2px black;
+`
+
+export const TableRow = styled.tr`
+    text-align: center;
+`
+
+export const TableHead = styled.th`
+    background-color: #FFE849;
+    border-bottom: solid 2px black;
+    padding: 10px 30px;
+`
+
+export const Cell = styled.td`
+    padding: 7px 12px;
+    background-color: #2a3338;
+    color: white;
+`
