a15084b2f125a5d229ae71811f44832c4752d2ab
German Diaz <germanrodrigo.2003@hotmail.com>
2021-07-21 18:12:19 -0500

    reparacion de listas(tablas)

--- src/components/pages/ListReport/ListReport.js
+++ src/components/pages/ListReport/ListReport.js
@@ -1,30 +1,37 @@
-import { Table, TableRow, TableHead, Cell } from "../../style/ListReportStyle";
+import { Table, Caption, Row, Head, Hcell, Cell } from "../../style/ListReportStyle";
 
 const ListReport = () => {
     return (
-        <Table>
-            <TableRow>
-                <TableHead>Nombre Completo</TableHead>
-                <TableHead>Fecha de perdida</TableHead>
-                <TableHead>Elemento</TableHead>
-                <TableHead>Descripcion</TableHead>
-                <TableHead>Contacto</TableHead>
-            </TableRow>
-            <TableRow>
-                <Cell>Jhon steven saavedra</Cell>
-                <Cell>24/08/2021</Cell>
-                <Cell>celular</Cell>
-                <Cell>un celular rojo con funda negra y al prenderlo tiene un fondo de pantalla con tonalidades rojas</Cell>
-                <Cell>121221212</Cell>
-            </TableRow>
-            <TableRow>
-                <Cell>German Rodrigo Diaz Olarte</Cell>
-                <Cell>15/09/2021</Cell>
-                <Cell>maleta</Cell>
-                <Cell>un celular rojo con funda negra y al prenderlo tiene un fondo de pantalla con tonalidades rojas</Cell>
-                <Cell>121221212</Cell>
-            </TableRow>
-        </Table>
+        <>
+            <Table>
+                <Caption>Lista de objetos</Caption>
+                <Head>
+                    <Row>
+                        <Hcell>Nombre Completo</Hcell>
+                        <Hcell>Fecha de perdida</Hcell>
+                        <Hcell>Elemento</Hcell>
+                        <Hcell>Descripcion</Hcell>
+                        <Hcell>Contacto</Hcell>
+                    </Row>
+                </Head>
+                <tbody>
+                    <Row>
+                        <Cell>Jhon steven saavedra</Cell>
+                        <Cell>24/08/2021</Cell>
+                        <Cell>celular</Cell>
+                        <Cell>un celular rojo con funda negra y al prenderlo tiene un fondo de pantalla con tonalidades rojas</Cell>
+                        <Cell>1222122122</Cell>
+                    </Row>
+                    <Row>
+                        <Cell>German Rodrigo Diaz Olarte</Cell>
+                        <Cell>15/09/2021</Cell>
+                        <Cell>maleta</Cell>
+                        <Cell>un celular rojo con funda negra y al prenderlo tiene un fondo de pantalla con tonalidades rojas</Cell>
+                        <Cell>1222122122</Cell>
+                    </Row>
+                </tbody>
+            </Table>
+        </>
     );
 }
  
--- src/components/style/ListReportStyle.js
+++ src/components/style/ListReportStyle.js
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+export const Caption = styled.caption`
+    margin-bottom: 30px;
+    font-weight: 800;
+    font-size: 50px;
+`
+
 export const Table = styled.table`
     border-radius: 2px;
     border-collapse: collapse;
@@ -7,17 +13,21 @@ export const Table = styled.table`
     margin-top: 5%;
 `
 
-export const TableRow = styled.tr`
+export const Row = styled.tr`
     text-align: center;
     &:hover {
         background-color: #4e575c;
     }
 `
 
-export const TableHead = styled.th`
+export const Head = styled.thead`
     background-color: #FFE849;
     border-bottom: solid 2px black;
     padding: 10px 30px;
+`
+
+export const Hcell = styled.th`
+    padding: 7px 12px;
 `
 
 export const Cell = styled.td`
